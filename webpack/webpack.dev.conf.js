e32d3ee951e917b1dd31ad57c6fc80d9c74dfb0d
HayashiMei <hayashi@kato-megumi.com>
2019-02-26 11:04:16 +0800

    Update: Update webpack configuration.

--- webpack/webpack.dev.conf.js
+++ webpack/webpack.dev.conf.js
@@ -2,7 +2,7 @@ const path = require('path');
 const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf');
 
-const webpackConfig = merge(baseWebpackConfig, {
+module.exports = merge(baseWebpackConfig, {
   output: {
     path: path.resolve(__dirname, '../dist/'),
     filename: 'js/[name].js',
@@ -12,5 +12,3 @@ const webpackConfig = merge(baseWebpackConfig, {
   devtool: 'source-map',
   watch: true,
 });
-
-module.exports = webpackConfig;
--- webpack/webpack.prod.conf.js
+++ webpack/webpack.prod.conf.js
@@ -4,21 +4,18 @@ const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const baseWebpackConfig = require('./webpack.base.conf');
 
-const webpackConfig = merge(baseWebpackConfig, {
+module.exports = merge(baseWebpackConfig, {
   output: {
     path: path.resolve(__dirname, '../dist/'),
     filename: 'js/[name].js',
     publicPath: '',
   },
   mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin(['css', 'js'], {
+      root: path.resolve(__dirname, '../dist/'),
+      verbose: true,
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+  ],
 });
-
-webpackConfig.plugins = webpackConfig.plugins.concat([
-  new CleanWebpackPlugin(['css', 'js'], {
-    root: path.resolve(__dirname, '../dist/'),
-    verbose: true,
-  }),
-  new webpack.optimize.ModuleConcatenationPlugin(),
-]);
-
-module.exports = webpackConfig;
